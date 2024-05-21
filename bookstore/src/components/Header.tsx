a9fa0a25362621802b0348f8713a5abb09f962df
naopeke <naopeke@protonmail.com>
2024-05-21 19:18:47 +0200

    first commit

---
+++ bookstore/src/components/Header.tsx
@@ -0,0 +1,92 @@
+// import React from 'react';
+import ".Header.css";
+
+function Header() {
+  console.log("Header works");
+
+  const anchoFoto = 300;
+
+  return (
+    <header className="header">
+      {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+            </svg>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">Perfil</a></li>
+                    <li><a href="/services">Libros</a></li>
+                    <li><a href="/contact">Añadir Libro</a></li>
+                    <li><a href="/contact">Editar Libro</a></li>
+                    <li><a href="/contact">Log Out</a></li>
+
+                </ul>
+            </nav> */}
+
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <img src="/img/foto-perro.jpg" width={anchoFoto}  alt="Logo" />
+          <span className="font-semibold text-xl tracking-tight">
+            BookStore
+          </span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Perfil
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Libros
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Añadir Libro
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Editar Libro
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Log Out
+            </a>
+          </div>
+          <div>
+            <a
+              href="#"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+              Download
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ bookstore/src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import '.Home.css';
+
+function Home (){
+    return (
+        <>
+        
+            <h1>Home</h1>
+
+            <div className="home__message--1">
+                "The only way to learn is to live"
+            </div>
+            <div className="home__message--2">
+                Matt Haig - The Midnight Library
+            </div>
+        </>
+    )
+}
+
+export default Home;
