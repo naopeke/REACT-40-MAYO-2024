8601d0f298700ed2028412cb0435ffdca0d54182
naopeke <naopeke@protonmail.com>
2024-05-22 19:44:50 +0200

    libros

--- bookstore/src/components/BookItem.tsx
+++ bookstore/src/components/BookItem.tsx
@@ -1,5 +1,7 @@
 // import styles from './BookItem.module.css';
 import { ReactNode } from 'react';
+import { FaTrash } from "react-icons/fa";
+import { CiEdit } from "react-icons/ci";
 
 type BookItemProps = {
     imageUrl: string;
@@ -24,17 +26,34 @@ function BookItem (props: BookItemProps){
         <div className="bookitem__container min-h-screen flex justify-center items-center pt-6">
            
             <div className="max-w-xm rounded overflow-hidden shadow-lg">
-                <img className="w-60" src={props.imageUrl} alt="image" />
+                <img className="w-60 ml-6" src={props.imageUrl} alt="image" />
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{props.title}</div>
-                    <p className="text-gray-700 text-base">
-                     {props.author}
-                     {props.price}
-                    </p>
+                    
+                    <div className="grid grid-cols-2 gap-2">
+                        <div className= "flex flex-col" >
+                            <p className="text-gray-700 text-base italic">
+                                {props.author}
+                            </p>
+                            <p className="text-gray-700 text-base">
+                                {props.price}€
+                            </p>
+                        </div>
+
+                    <div className= "flex flex-col items-end">
+                        <div>
+                        <span className="inline-block bg-gray-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Novela</span>
+                        </div>
+                        <div className="flex flex-row space-x-10">
+                            <CiEdit style={{ color: 'blue'}}/><FaTrash style={{ color: 'red'}}/>
+                        </div>
+                    </div>
+
+                    </div>
+
                 </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Novela</span>
-                </div>
+ 
+  
                 </div>
         </div>
     )
--- bookstore/src/pages/BooksPage.tsx
+++ bookstore/src/pages/BooksPage.tsx
@@ -1,7 +1,5 @@
 // import styles from './BooksPage.module.css';
 
-import './BooksPage.css';
-
 import BookItem from "../components/BookItem";
 
 function BooksPage (){
@@ -10,28 +8,14 @@ function BooksPage (){
     //     { imageUrl: 'http', title: 'Clean JavaScript', author:'Miguel A. Gómez', price: 20},
     //     { imageUrl: 'http', title: 'Clean JavaScript', author:'Miguel A. Gómez', price: 20},
     // ]
+
     return (
         <div className="bookspage__container">
         
-        <BookItem imageUrl="https://m.media-amazon.com/images/I/51q4UQsslIL._SL1233_.jpg" title="Clean JavaScript" author="Miguel A. Gómez"/>
+        <BookItem imageUrl="https://m.media-amazon.com/images/I/51q4UQsslIL._SL1233_.jpg" title="Clean JavaScript" author="Miguel A. Gómez" price={29.9} />
 
-        {/* <div className="books-page">
-      {books.map((book) => (
-        <BookItem key={book.title} {...book} onClick={() => handleBookClick(book)} />
-      ))}
-    </div> */}
 
-    
 
-        {/* <BookItem title="Never ending story" price={10}>
-            <h2>Soy un h2</h2>
-            <h3>Soy un h3</h3>
-        </BookItem>
-           
-        <BookItem title="necesita nombre">
-            Children item
-            <input type="text" ></input>
-        </BookItem> */}
         </div>
     )
 }
